842cb4ca4170af69bd605a87d3232ea208b4707f
Vyacheslav <jantschuk@gmail.com>
2020-04-22 11:45:44 +0300

    dist2

---
+++ precache-manifest.beffa261fa91b55e0d0207cf0aebd5ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0fbf5d6f948b122d4b06fe7edd8c5e2",
+    "url": "/cinema-app-dist/index.html"
+  },
+  {
+    "revision": "01e7d03954df1d81e6f1",
+    "url": "/cinema-app-dist/static/css/2.a72c35d3.chunk.css"
+  },
+  {
+    "revision": "83d0a42777f4a3bbac41",
+    "url": "/cinema-app-dist/static/css/main.292f671a.chunk.css"
+  },
+  {
+    "revision": "01e7d03954df1d81e6f1",
+    "url": "/cinema-app-dist/static/js/2.0dd0f2b2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/cinema-app-dist/static/js/2.0dd0f2b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83d0a42777f4a3bbac41",
+    "url": "/cinema-app-dist/static/js/main.d5e968ff.chunk.js"
+  },
+  {
+    "revision": "0d50209ec7ed0a0ca723",
+    "url": "/cinema-app-dist/static/js/runtime-main.94ff7abb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ee8d92c65d55b4218ff0f4aab8c8b13e.js"
+  "/cinema-app-dist/precache-manifest.beffa261fa91b55e0d0207cf0aebd5ac.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cinema-app-dist/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
